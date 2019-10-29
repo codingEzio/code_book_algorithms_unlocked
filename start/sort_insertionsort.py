58447e2972a34f0af958fcabce230266dd73b3b2
Ezio <assassinste@gmail.com>
2019-10-29 17:56:08 +0800

    Refined (added notes & new variable names)

--- start/sort_insertionsort.py
+++ start/sort_insertionsort.py
@@ -1,14 +1,22 @@
 from annotated_types import IntList
 
 
+# O(n²) typically
 def insertion_sort(arr: IntList):
-    for idx in range(1, len(arr)):
-        current_val = arr[idx]
-        i = idx - 1
-        while i >= 0 and current_val < arr[i]:
-            arr[i+1] = arr[i]
-            arr[i] = current_val
-            i = i - 1
+    """The speed of this algorithm is also not that good as well,
+    the only thing this algo does is compare the current element with
+    the previous elem, then decide swap them or not (I thought it's hard..).
+    """
+
+    # I know the names I used here might be a little confusing to you,
+    # Umm.. sorry!
+    for idx_add1 in range(1, len(arr)):
+        val_adi1 = arr[idx_add1]
+        idx_orig = idx_add1 - 1
+        while idx_orig >= 0 and val_adi1 < arr[idx_orig]:
+            arr[idx_orig+1] = arr[idx_orig]
+            arr[idx_orig] = val_adi1
+            idx_orig = idx_orig - 1
     return arr
 
 
