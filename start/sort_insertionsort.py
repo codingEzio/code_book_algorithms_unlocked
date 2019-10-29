33d687f2e14ac88b7762c49e3c8aa817e85f0ff6
Ezio <assassinste@gmail.com>
2019-10-29 18:23:25 +0800

    Two helper functions added

--- start/sort_insertionsort.py
+++ start/sort_insertionsort.py
@@ -1,4 +1,15 @@
 from annotated_types import IntList
+import bisect
+from random import choices
+
+
+def randomize_inputs(arr: IntList):
+    length: int = len(arr)
+    return choices(arr, k=length)
+
+
+def sort_inputs_by(func_name, arr: IntList):
+    return func_name(arr)
 
 
 # O(n²) typically
@@ -13,14 +24,26 @@ def insertion_sort(arr: IntList):
     for idx_add1 in range(1, len(arr)):
         val_adi1 = arr[idx_add1]
         idx_orig = idx_add1 - 1
-        while idx_orig >= 0 and val_adi1 < arr[idx_orig]:
-            arr[idx_orig+1] = arr[idx_orig]
+        while idx_orig >= 0 and arr[idx_orig] > val_adi1:
+            arr[idx_orig + 1] = arr[idx_orig]
             arr[idx_orig] = val_adi1
             idx_orig = idx_orig - 1
     return arr
+    pass
+
+
+# O(n²) typically
+def insertion_sort_bisect(arr: IntList):
+    pass
+
+
+# O(n²) typically
+def insertion_sort_binsearch(arr: IntList):
+    pass
 
 
 if __name__ == "__main__":
     inputs: IntList = [6, 7, 8, 3, 4, 5]
-    sorted_inputs: IntList = insertion_sort([6, 7, 8, 3, 4, 5])
-    assert insertion_sort(inputs) == sorted_inputs
+    sorted_inputs: IntList = [3, 4, 5, 6, 7, 8]
+
+    assert sort_inputs_by(insertion_sort, arr=inputs) == sorted_inputs
