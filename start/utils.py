c30a96d40cc5c2d11e1affa6601cc90b3facced4
Ezio <assassinste@gmail.com>
2019-10-30 09:55:55 +0800

    Rewrite my own random function

--- start/utils.py
+++ start/utils.py
@@ -1,11 +1,15 @@
-from random import choices
-from annotated_types import IntList
+from random import shuffle
+from annotated_types import IntList, Function
 
 
-def randomize(arr: IntList):
-    length: int = len(arr)
-    return choices(arr, k=length)
+def randomize(arr: IntList) -> None:
+    """Randomize an array/list in place.
+    Reference: https://stackoverflow.com/a/976921
+    """
+    return shuffle(arr)
 
 
-def sort_inplace(func, arr: IntList):
+def sort_inplace(func: Function, arr: IntList):
+    """Sort an array/list in place
+    """
     return func(arr)
