cba7a72b71d384e92b2564bf4a8d75d87d5b9bf7
Ezio <assassinste@gmail.com>
2019-10-30 21:55:34 +0800

    Added 2nd merge sort implementation (no lib is used)

--- start/sort_mergesort.py
+++ start/sort_mergesort.py
@@ -3,6 +3,44 @@ from annotated_types import IntList
 from utils import randomize
 
 
+def _merge(left: IntList, right: IntList) -> IntList:
+    result: IntList = []
+
+    left_idx: int
+    right_idx: int
+    left_idx, right_idx = 0, 0
+    while left_idx < len(left) and right_idx < len(right):
+        if left[left_idx] <= right[right_idx]:
+            result.append(left[left_idx])
+            left_idx += 1
+        else:
+            result.append(right[right_idx])
+            right_idx += 1
+
+    if left_idx < len(left):
+        result.extend(left[left_idx:])
+    if right_idx < len(right):
+        result.extend(right[right_idx:])
+
+    return result
+
+
+def merge_sort_native(arr: IntList) -> IntList:
+    """Merge sort based on native `merge` implementation.
+    """
+    if len(arr) <= 1:
+        return arr
+
+    mid: int = len(arr) // 2
+    left: IntList = arr[:mid]
+    right: IntList = arr[mid:]
+
+    left = merge_sort_native(left)
+    right = merge_sort_native(right)
+
+    return list(_merge(left, right))
+
+
 def merge_sort_heapq(arr: IntList) -> IntList:
     """Merge sort based on `heapq.merge`.
     """
@@ -25,3 +63,6 @@ if __name__ == "__main__":
 
     randomize(inputs)
     assert merge_sort_heapq(inputs) == sorted_inputs
+
+    randomize(inputs)
+    assert merge_sort_native(inputs) == sorted_inputs
