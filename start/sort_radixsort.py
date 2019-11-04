14b49a0d85894d0cd143774fd5f27052a0baa115
Ezio <assassinste@gmail.com>
2019-11-04 21:49:32 +0800

    Another implemention on radix sort

--- start/sort_radixsort.py
+++ start/sort_radixsort.py
@@ -2,10 +2,83 @@ from src.annotated_types import IntList, NestedIntList
 from src.test_utils import randomize
 
 
+# util functions for `radix_sort_2nd`.
+def radix_get_length(value: int) -> int:
+    """# Returns the length, in number of digits, of value
+    """
+    if value == 0:
+        return 1
+
+    digits: int = 0
+    while value != 0:
+        digits += 1
+        value //= 10
+
+    return digits
+
+
+# util functions for `radix_sort_2nd`.
+def radix_get_max_length(numbers: IntList) -> int:
+    """Returns the maximum length, in number of digits, out of all list elements
+    """
+    max_digits: int = 0
+    num: int
+
+    for num in numbers:
+        digit_count: int = radix_get_length(num)
+        if digit_count > max_digits:
+            max_digits = digit_count
+
+    return max_digits
+
+
+# https://derka.space/RefrencePage/DataStructuresZy/SortingAlgorithms/Python_RadixSort/SortingAlgorithms_Python_RadixSort_s14.php
+def radix_sort_2nd(numbers: IntList) -> IntList:
+    """Yet another implementation on radix sort.
+    """
+    buckets: NestedIntList
+    max_digits: int
+    num: int
+    pow_10: int = 1
+    bucket_index: int
+    bucket: IntList
+    negatives: IntList
+    non_negatives: IntList
+
+    buckets = [[] * 10]
+    max_digits = radix_get_max_length(numbers)
+
+    for digit_index in range(max_digits):
+        for num in numbers:
+            bucket_index = (num // pow_10) % 10
+            buckets[bucket_index].append(num)
+
+        numbers.clear()
+
+        for bucket in buckets:
+            numbers.extend(bucket)
+            bucket.clear()
+
+        pow_10 = pow_10 * 10
+
+    negatives = []
+    non_negatives = []
+    for num in numbers:
+        if num < 0:
+            negatives.append(num)
+        else:
+            non_negatives.append(num)
+
+    negatives.reverse()
+    numbers.clear()
+    numbers.extend(negatives + non_negatives)
+
+    return numbers
+
+
+# http://qaru.site/questions/15331230/radix-sort-python-string
 def radix_sort(arr: IntList, base=10):
     """A simple implementation of radix sort (order: DESC).
-    References:
-        http://qaru.site/questions/15331230/radix-sort-python-string
     """
 
     def list_to_buckets(_arr: IntList, _base: int, _iteration: int):
