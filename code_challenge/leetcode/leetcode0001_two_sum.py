095b5f5a0562d8c990b6004f28e51abc91b0c3b2
Ezio <assassinste@gmail.com>
2021-05-12 07:16:12 +0800

    Leetcode 0001: solved by myself, submitted

--- code_challenge/leetcode/leetcode0001_two_sum.py
+++ code_challenge/leetcode/leetcode0001_two_sum.py
@@ -1,30 +1,22 @@
 # Status
-#   solved by myself        : no
-#   submitted to leetcode   : no
-from typing import List, Dict
+#   solved by myself        : yes
+#   submitted to leetcode   : yes
 
+def two_sum(nums_arr, target):
+    if len(nums_arr) <= 1:
+        return []
 
-def two_sum(nums: List[int], target: int):
-    """
-    Giving a list [9, 7, 3, 1, 5] an integer 10,
-    it should produce something like [7, 3] (an list of indexes).
-    """
-    # e.g. []
-    num_to_index: Dict[int, int] = {}
+    dict_item_as_key = dict()
 
-    idx: int
-    num: int
-    for idx, num in enumerate(nums):
-        if target - num in num_to_index:
-            return [num_to_index[target - num], idx]
+    for idx, item in enumerate(nums_arr):
+        remainder = target - item
 
-        num_to_index[num] = idx
+        if remainder in dict_item_as_key:
+            return [dict_item_as_key[remainder], idx]
+        else:
+            dict_item_as_key[item] = idx
 
-    return []
-
-
-assert two_sum([9, 7, 3, 1], 10) == [1, 2]  # not 0,3 🤔
-assert two_sum([9, 1, 3, 1], 10) == [0, 1]
-assert two_sum([9, 2, 3, 1], 10) == [0, 3]
-assert two_sum([9, 0, 1], 10) == [0, 2]
-assert two_sum([1], 1) == []
+assert two_sum([9, 3, 7, 1], 10) == [1, 2]
+assert two_sum([9, 1, 9, 1], 10) == [0, 1]
+assert two_sum([10], 10) == []
+assert two_sum([], 10) == []
