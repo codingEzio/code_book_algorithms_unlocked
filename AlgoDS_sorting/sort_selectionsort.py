f508b55a684591a9f5ac052d51bf38156e428f4c
CodingEzio <assassinste@gmail.com>
2022-04-18 13:05:55 +0800

    Added Selection Sort (detailed comments)

--- AlgoDS_sorting/sort_selectionsort.py
+++ AlgoDS_sorting/sort_selectionsort.py
@@ -1,16 +1,54 @@
 from utils.annotated_types import IntList
 
 
-# O(n²) typically
-def selection_sort(arr: IntList):
-    # 1. e.g. [7,2,3] -> [2,7,3] -> [2,3,7]
-    # 2. [..[..[..]]] (0,rest) (1,rest) (push the smallest to the left-side)
-    for i, e in enumerate(arr):
-        mn: IntList = min(range(i, len(arr)), key=arr.__getitem__)
-        arr[i], arr[mn] = arr[mn], e
-    return arr
+class Solution:
+    def selection_sort(self, nums: IntList) -> IntList:
+        """
+        Requirements: Integer list AND sorted by ASC
+
+        The outer loop tries to compare one element with every other
+        elements. When the comparison is done, move it out of our way,
+        which means directly switching the position (move the smallest
+        to the front), then we compare the element after it along with
+        the element after the elements after the "new" base element to
+        compare.
+        """
+        length = len(nums)
+
+        if length == 0:
+            return []
+
+        for asc_left in range(0, length - 1):
+            # since we're sorting in ascending order, if the left is the
+            # the smaller one, we'll keep it that way (like a default).
+            smallest = asc_left
+
+            for asc_right in range(asc_left + 1, length):
+
+                # if we didn't see the default (left is smaller), switch
+                # the position ('smallest' underneah is still the left)
+                if nums[smallest] > nums[asc_right]:
+                    smallest = asc_right
+
+            # finished comparing the base element with every other elements
+            # if the left is indeed smaller, we'll just go to the next loop,
+            # if it isn't, move the smaller one to the front (also kind move
+            # it out of our way for the comparisons for the remaining elements
+            if smallest != asc_left:
+                nums[smallest], nums[asc_left] = nums[asc_left], nums[smallest]
+
+        return nums
 
 
 if __name__ == "__main__":
-    inputs: IntList = [6, 7, 8, 3, 4, 5]
-    assert selection_sort(inputs) == [3, 4, 5, 6, 7, 8]
+    E = {
+        "c1": {"in": [-1, -20, 9, 5], "out": [-20, -1, 5, 9]},
+        "c2": {"in": [-5, -2, -1, 4], "out": [-5, -2, -1, 4]},
+        "c3": {"in": [0], "out": [0]},
+    }
+
+    run = Solution()
+
+    assert run.selection_sort(E["c1"]["in"]) == E["c1"]["out"]
+    assert run.selection_sort(E["c2"]["in"]) == E["c2"]["out"]
+    assert run.selection_sort(E["c3"]["in"]) == E["c3"]["out"]
